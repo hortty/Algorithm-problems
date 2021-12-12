690c80571fe52581f340dce17e068b5022f1ab48
Gabriel Bueno <gabrielhortmann@gmail.com>
2021-12-12 00:37:12 -0300

    URI

---
+++ 1000 Hello World.cs
@@ -0,0 +1,24 @@
+/*
+URI Problem: 1000 
+Hello World!
+
+Bem-vindo ao beecrowd!
+
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+*/
+
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        Console.WriteLine("Hello World!");
+    }
+
+}
